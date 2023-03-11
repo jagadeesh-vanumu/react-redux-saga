59ebdf9914a15da64e1b6c6cbd271c22f3e0fc40
jagadeesh-vanumu <jagadeshvanumu@gmail.com>
2023-03-11 11:34:13 +0530

    configure saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 // import {createStore} from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import root_reducer from './root_reducer';
+import productSaga from './productSaga';
+import createSagaMiddleware from '@redux-saga/core';
 
 
-const store = configureStore({reducer:root_reducer}); 
+const sagaMiddleware = createSagaMiddleware();
+
+
+const store = configureStore({
+    reducer:root_reducer,
+    middleware: () => [sagaMiddleware]
+}); 
+
+sagaMiddleware.run(productSaga)
 
 export default store;
